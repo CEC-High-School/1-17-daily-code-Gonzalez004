fb3b34ba9a95a3e714b63ae7fd36edd24f1ee0ac
Dark Stallion <gonzalez.ferny31@outlook.com>
2018-01-17 08:00:43 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "A man walked into an heavy metal bar" << endl;
+	system("pause");
+	cout << endl;
+	cout << "And he said owwwwwwwwwww" << endl;
+	return 0;
+}
